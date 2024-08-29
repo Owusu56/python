8e098b3c1e797cd3ac0c5b935da43876da3f954c
Owusu56 <165980842+Owusu56@users.noreply.github.com>
2024-08-29 13:33:32 -0700

    con commit

---
+++ Functions/Creating.py
@@ -0,0 +1,21 @@
+#Creating a function
+def Bro():
+    pass
+
+#Defining a function
+def Bro():
+    print("I'm a non returning function")
+    print("Byeeeeeeeeeeeeeeeeeeeeeeeeee")
+
+#Calling the function
+Bro()
+
+#Passing in an argument
+def Bro(args):
+    print(args)
+    Bro("Non Returning Passing in an argument")
+
+#Return Function
+def Bro(args):
+    return args
+print(Bro("Passing in an argument"))
