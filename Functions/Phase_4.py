c9df0fdf14eeb3ec49ea615bc5657dc791648021
Owusu56 <165980842+Owusu56@users.noreply.github.com>
2024-09-04 13:16:38 -0700

    con commit

---
+++ Functions/Phase_4.py
@@ -0,0 +1,19 @@
+def TrafficLight(colour, time):
+    print("The", colour, " light is on")  # digitalWrite(color,HIGH)
+    print("Delay for ", time, " ms")  # delay(10000)
+    print("The light is", colour, )  # digitalWrite(color,HIGH)
+    print("Delay for ", "10000", " ms")  # delay(10000)
+
+
+TrafficLight("Red", 10000)
+TrafficLight("Blinking", 50)
+TrafficLight("Blinking", 50)
+TrafficLight("Blinking", 50)
+TrafficLight("Yellow", 5000)
+TrafficLight("Blinking", 50)
+TrafficLight("Blinking", 50)
+TrafficLight("Blinking", 50)
+TrafficLight("Green", 15000)
+TrafficLight("Blinking", 50)
+TrafficLight("Blinking", 50)
+TrafficLight("Blinking", 50)
